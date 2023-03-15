27c707f62c5e536b338ad46766aa2bc87df90d5f
Sina Moradian <sinacsharp@gmail.com>
2023-03-16 01:14:54 +0330

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenseApp from "./Components/ExpenseApp";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h2>Expense Tracker</h2>
+      </header>
+      <ExpenseApp />
+    </div>
+  );
+};
+
+export default App;
