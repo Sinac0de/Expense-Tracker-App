e5bf75e8a3f2abaf107e28762da1c0d1291d2ed6
Sina Moradian <sinacsharp@gmail.com>
2023-03-16 02:29:21 +0330

    Add Overview & Transactions components

--- src/Components/ExpenseApp.js
+++ src/Components/ExpenseApp.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import TransactionsCompoenent from "./TransactionsComponent";
+import OverviewComponent from "./OverviewComponent";
+
 const ExpenseApp = () => {
-  return <section>Expense App</section>;
+  const [expense, setExpense] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [transactions, setTransactions] = useState([]);
+
+  return (
+    <section>
+      <OverviewComponent income={income} expense={expense} />
+      <TransactionsCompoenent transactions={transactions} />
+    </section>
+  );
 };
 
 export default ExpenseApp;
