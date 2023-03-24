802e6a604d88c72721e2a7b8d0d3daec837736d1
Sina Moradian <sinacsharp@gmail.com>
2023-03-24 16:52:26 +0330

    Add search to the transactions section

--- src/Components/TransactionsComponent.js
+++ src/Components/TransactionsComponent.js
@@ -1,17 +1,55 @@
+import { useEffect, useState } from "react";
+
 const TransactionsComponent = ({ transactions }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredTrans, setFilteredTrans] = useState(transactions);
+
+  const filterTransactions = (search) => {
+    if (!search) {
+      setFilteredTrans(transactions);
+      return;
+    }
+    const filtered = transactions.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredTrans(filtered);
+  };
+
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transactions]);
+
+  if (!transactions.length) return <h4>Add some transactions</h4>;
+
   return (
     <section>
       <h3 className="sectionHeader">Transactions</h3>
-      {transactions.map((t) => (
-        <div
-          key={t.id}
-          className="transaction"
-          style={{ borderRight: t.type !== "expense" && "5px solid green" }}
-        >
-          <span>{t.desc}</span>
-          <span>${t.amount}</span>
-        </div>
-      ))}
+      <input
+        type="search"
+        className="searchInput"
+        value={searchItem}
+        onChange={changeHandler}
+        placeholder="Search for a transaction"
+      />
+      {filteredTrans.length ? (
+        filteredTrans.map((t) => (
+          <div
+            key={t.id}
+            className="transaction"
+            style={{ borderRight: t.type !== "expense" && "5px solid green" }}
+          >
+            <span>{t.desc}</span>
+            <span>${t.amount}</span>
+          </div>
+        ))
+      ) : (
+        <p className="searchNotFound">No items match!</p>
+      )}
     </section>
   );
 };
